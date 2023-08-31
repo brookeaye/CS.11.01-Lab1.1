c0d80aa8f79fa08105fae6ce3bede828434deb72
brookeaye <brookey2026@student.cis.edu.hk>
2023-08-31 14:45:29 +0800

    woohoo

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
         Scanner ageScanner = new Scanner(System.in);
-        System.out.printIn("Enter your age, bozo.");
-        Int age = ageScanner.nextLine();
-        System.out.printIn(age);
-    
+        System.out.println("Enter your age bozo");
+        int age = ageScanner.nextInt();
+        System.out.println("You should date someone who is at least " + (7 + age/2) + " years old");
+        System.out.println("Though I also believe that you should have the freedom to choose. So do whatever you want, but also don't be a perv. Ok.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age.");
+        int yourAge = scanner.nextInt();
+        System.out.println("You should read " + (100 - yourAge) + "  pages of a book before giving up on it.");
     }
 }
