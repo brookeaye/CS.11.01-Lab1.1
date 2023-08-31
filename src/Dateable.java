af6fe4f7f02eb5d6f29e138482c6a1d091114e03
brookeaye <143502231+brookeaye@users.noreply.github.com>
2023-08-31 14:20:26 +0800

    Dateable.java

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,10 @@
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner ageScanner = new Scanner(System.in);
+        System.out.printIn("Enter your age, bozo.");
+        Int age = ageScanner.nextLine();
+        System.out.printIn(age);
+    
     }
 }
